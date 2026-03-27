7a5708f29b4bca182a1177d5b8b86956168ada71
CodeSuit <237616418+ShankarTech-web@users.noreply.github.com>
2026-03-27 17:36:11 +0530

    Added negative and empty login test cases using JSON data

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,10 +1,33 @@
 const { test, expect } = require('@playwright/test');
 const LoginPage = require('../pages/LoginPage');
+const user = require('../test-data/user.json');
 
-test('Verify login page loads', async ({ page }) => {
+
+test('Valid Login', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.open();
+  await loginPage.login(user.validUser.email, user.validUser.password);
+
+  await expect(page).not.toHaveURL(/login.php/);
+});
+
+
+test('Invalid Login', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.open();
+  await loginPage.login(user.invalidUser.email, user.invalidUser.password);
+
+  await expect(page).toHaveURL(/login.php/);
+});
+
+
+test('Empty Login', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.open();
+  await loginPage.login(user.emptyUser.email, user.emptyUser.password);
 
   await expect(page).toHaveURL(/login.php/);
 });
